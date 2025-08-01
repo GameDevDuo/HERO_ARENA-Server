e06d2c1a00aff21082ae3a3b40fa62b6beb23a68
2rjswn <h47841930@gmail.com>
2025-08-02 04:20:03 +0900

    edit deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,4 @@ docker build . -t heroarena/heroarena-server:latest
 docker stop heroarena-server
 docker rm heroarena-server
 
-docker run -d -p 8080:8080 --name heroarena-server --env-file ./.env heroarena/heroarena-server:latest
+docker run -d --name heroarena-server --network heroarena-net -p 80:8080 --env-file .env heroarena/heroarena-server:latest
